cd1a179336f3588a28e470e7bd99e0426ba960e4
Ye Joo Park <subwaymatch@gmail.com>
2019-05-21 10:44:35 -0700

    Remove button text and use tooltip instead

--- src/components/PaletteButtons.jsx
+++ src/components/PaletteButtons.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { paletteRandomize } from "../actions";
+import "react-tippy/dist/tippy.css";
+import { Tooltip } from "react-tippy";
 
 class PaletteButtons extends Component {
   render() {
@@ -8,15 +10,33 @@ class PaletteButtons extends Component {
 
     return (
       <div id="palette-buttons-wrapper">
-        <button onClick={dispatchPaletteRandomize}>
-          <i className="icon ion-ios-shuffle" />
-          <span>Randomize</span>
-        </button>
+        <Tooltip
+          animation="shift"
+          duration={200}
+          animateFill={false}
+          hideOnClick={false}
+          title="Randomize Palette"
+          position="bottom"
+          distance={-10}
+        >
+          <div className="button" onClick={dispatchPaletteRandomize}>
+            <i className="icon ion-ios-shuffle" />
+          </div>
+        </Tooltip>
 
-        <button>
-          <i className="icon ion-ios-link" />
-          <span>Link</span>
-        </button>
+        <Tooltip
+          animation="shift"
+          duration={200}
+          animateFill={false}
+          hideOnClick={false}
+          title="Copy Link to Clipboard"
+          position="bottom"
+          distance={-10}
+        >
+          <div className="button">
+            <i className="icon ion-ios-link" />
+          </div>
+        </Tooltip>
       </div>
     );
   }
